f8512b2c44e3056de9d6224dcbf2a7c7cf49107a
benjaehyun <139726967+benjaehyun@users.noreply.github.com>
2023-12-14 20:19:47 -0800

    reconfigured projects page to use ProjectsList component

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,8 +1,39 @@
 import { Link } from "react-router-dom"
+import ProjectsList from "../../components/ProjectsList/ProjectsList"
 
 
 export default function ProjectsPage () {
 
+    const projects = [
+        {
+            title: "lowKEY",
+            description: "A MERN-Stack dating app. Full-Stack application where users can connect their Spotify account to view other users who have similar tastes in music. With the Spotify API, song metrics are used to sort users based on similarity. Once matched, users can chat privately and view what musical features they have in common.",
+            images: ["lowkey/lowkey-mac-1.png", "lowkey/lowkey-mac-2.png"],
+            linkSuffix: "lowkey"
+        },
+        {
+            title: "The Draft",
+            description: "A Django forum app. Full-Stack application created in collaboration with developers Nadew Demissew and Jon Do. The Draft is a discussion forum inspired by websites such as 'CollegeConfidential', with its focus shifted toward navigating the complex and challenging world of job hunting. This app also features a job application tracker which helps users track a particular application cycle.",
+            images: ["thedraft/thedraft-mac-1.png", "thedraft/thedraft-mac-2.png"],
+            linkSuffix: "thedraft"
+        },
+        {
+            title: "cardIO",
+            description: "A MEN-Stack flashcard app. My first Full-Stack application: CardIO. This project was developed as an educational resource to draw on high-yield learning from spaced repetition of flashcards. The idea for this app comes from my past experiences of using other flashcard apps such as Quizlet and Anki back as a pre-med student.",
+            images: ["cardio/cardio-mac-1.png", "cardio/cardio-mac-2.png"],
+            linkSuffix: "cardio"
+        },
+        {
+            title: "sweeper",
+            description: "A browser game made using JavaScript, HTML, and CSS. Based on the popular game Minesweeper, sweeper was my first dive into development, while using a nostalgic pastime as reference. My first coding endeavor utilizes basic game logic, DOM manipulation for graphic rendering, and a recursive implementation for the cell reveal function.",
+            images: [
+                // "sweeper/sweeper-1.png", "sweeper/sweeper-2.png", 
+            // "sweeper/sweeper-3.png", "sweeper/sweeper-4.png"
+        ],
+            linkSuffix: "sweeper"
+        },
+    ];
+    
     return (
         <>
             <div 
@@ -19,7 +50,10 @@ export default function ProjectsPage () {
              <h2 className="text-white text-2xl" >
              Take a look at what I've been up to recently ⬇️ 
              </h2>
-            <div className='flex flex-col justify-evenly items-center mb-[15vh] space-y-12 font-mono mt-[30vh]'>
+
+             <ProjectsList projects={projects} />
+
+            {/* <div className='flex flex-col justify-evenly items-center mb-[15vh] space-y-12 font-mono mt-[30vh]'>
                 <h1 className="text-4xl text-white"> Projects Page</h1>
                 <Link to='/projects/lowkey' > 
                     <div className='text-white flex border-solid rounded-lg border-2 p-3 w-[40vw]'>
@@ -93,7 +127,7 @@ export default function ProjectsPage () {
                         </div>
                     </div>
                 </Link>
-            </div>
+            </div> */}
         </>
     )    
 }
